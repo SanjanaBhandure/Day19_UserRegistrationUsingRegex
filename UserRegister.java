f8f68d892ff757b7e1cfa5c9692b1cc36cea92be
SanjanaBhandure <35860614+SanjanaBhandure@users.noreply.github.com>
2022-05-01 01:09:37 +0530

    Added : As a User need to enter a valid email.

--- UserRegister.java
+++ UserRegister.java
@@ -11,14 +11,14 @@ public class UserRegister {
     }
 
     /*
-     * lastName method created in which Last Name starts with Cap and has minimum 3 Characters.
+     * isValidlastName method created in which Last Name starts with Cap and has minimum 3 Characters.
      */
     public static boolean isValidLastName(String lastName) {
         return Pattern.matches("^[A-Z]{1}[a-z]{2,}$", lastName);
     }
 
     /*
-     *
+     *  isValidEmail As a User need to enter a valid email.
      */
     public static boolean isValidEmail(String email) {
         return Pattern.matches("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$", email);
