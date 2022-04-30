38f3829aea708703ebde709a2a1a582a495cc841
SanjanaBhandure <35860614+SanjanaBhandure@users.noreply.github.com>
2022-05-01 01:54:01 +0530

    Added: As a User need to enter a valid First Name

--- test/java/com/brigelabz/UserRegister/UserRegisterTestCase.java
+++ test/java/com/brigelabz/UserRegister/UserRegisterTestCase.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class UserRegisterTestCase {
 
     /*
-     * isValidFirstNameTestCase
+     * isValidFirstNameTestCase created to validate first name.
      */
     @Test
     public void isValidFirstNameTestCase() {
