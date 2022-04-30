a3c7c194f1cd93d98386e821899a3776cbbb5926
SanjanaBhandure <35860614+SanjanaBhandure@users.noreply.github.com>
2022-05-01 01:11:35 +0530

    Added: As a User need to enter a valid Email

--- UserRegister.java
+++ UserRegister.java
@@ -18,7 +18,7 @@ public class UserRegister {
     }
 
     /*
-     *  isValidEmail As a User need to enter a valid email.
+     *  isValidEmail method created to clear all email samples provided separately.
      */
     public static boolean isValidEmail(String email) {
         return Pattern.matches("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$", email);
