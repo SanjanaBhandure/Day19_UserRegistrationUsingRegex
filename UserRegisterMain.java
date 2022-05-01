643d6438f756ff329b140c3240944d0030dca4ab
SanjanaBhandure <sweetybhandure@gmail.com>
2022-05-01 12:08:40 +0530

    Added:Should clear all email samples provided separately

--- UserRegisterMain.java
+++ UserRegisterMain.java
@@ -1,11 +1,10 @@
 /*
  * The User Registration System program implements an application that needs
  * to ensure all validations are in place during the User Entry.
- * UC5: As a User need to follow pre-defined Password rules.
- * Rule4 – Has exactly Special Character
+ * UC9: Should clear all email samples provided separately.
  *
  * @author: Sanjana Bhandure
- * @version: 1.8
+ * @version: 1.9
  * @date: 1-05-2022
  */
 
@@ -16,25 +15,8 @@ import java.util.Scanner;
 public class UserRegisterMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter First Name: ");
-        String firstName = scanner.nextLine();
-        // method calling to check whether the name is written in proper way or not.
-        System.out.println(UserRegister.isValidFirstName(firstName));
-
-        System.out.println("Enter Last Name: ");
-        String lastName = scanner.nextLine();
-        System.out.println(UserRegister.isValidLastName(lastName));
-
         System.out.println("Enter Email: ");
         String email = scanner.nextLine();
         System.out.println(UserRegister.isValidEmail(email));
-
-        System.out.println("Enter phone Number with country code(space between country code and phone Number): ");
-        String phoneNumber = scanner.nextLine();
-        System.out.println(UserRegister.isValidPhoneNumber(phoneNumber));
-
-        System.out.println("Enter Password: ");
-        String password = scanner.nextLine();
-        System.out.println(UserRegister.isValidPassword(password));
     }
 }
